91c9fb0b5204e5edb53d6454c8e2510b6d0eb75f
Geeta Ammanola <ammanola.geeta@gmail.com>
2025-09-01 16:33:43 +0530

    started

---
+++ ga_assistant/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ga_assistant',
+  webDir: 'www'
+};
+
+export default config;
